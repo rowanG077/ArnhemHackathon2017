701d15dbefbc0d63c09c4f6e70076fa028a4f3e7
Paul Tax <paultax@gmail.com>
2017-09-17 13:12:34 +0200

    Add

---
+++ SataliteGazer/Assets/DatehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Assets;
+
+public class DatehaviourScript : MonoBehaviour {
+	private CoordinateCalculator mainCalculator;
+	private Text txtDate;
+
+	// Use this for initialization
+	void Start () {
+		this.mainCalculator = GameObject.Find ("Main Camera").GetComponent<GameManager> ().Calculator;
+		this.txtDate = this.gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var sDate = this.mainCalculator.ReferenceTime.ToString ("HH:mm:ss dddd d MMMM");
+		this.txtDate.text = sDate;
+	}
+}
